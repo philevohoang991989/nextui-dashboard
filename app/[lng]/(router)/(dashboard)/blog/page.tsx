7e0b145209a6d3d64112f28425f41141e157b045
Le Vo Hoang Phi <levohoangphi@HoangPhi.local>
2023-12-26 17:53:05 +0700

    fix responsive

--- app/[lng]/(router)/(dashboard)/blog/page.tsx
+++ app/[lng]/(router)/(dashboard)/blog/page.tsx
@@ -11,7 +11,7 @@ export default function BlogPage ()  {
       <div className='container flex items-center justify-center'>
         <Card className='py-4 lg:w-3/4 xl:w-1/2'>
           <CardBody className='overflow-visible py-2'>
-            <div className='flex gap-6'>
+            <div className='flex gap-6 sm:flex-col md:flex-col lg:flex-row'>
               <Image alt='Shoe' className='flex-1 object-cover' src={shoe} />
               <div className='flex-1'>
                 <h2 className='text-lg font-bold uppercase'>
