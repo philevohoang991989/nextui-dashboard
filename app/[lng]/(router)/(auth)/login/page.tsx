d1b30eae162feb4d5b76f50bcefb93a951fceb8f
Le Vo Hoang Phi <levohoangphi@HoangPhi.local>
2023-12-26 14:57:43 +0700

    update zod and react hook form

--- app/[lng]/(router)/(auth)/login/page.tsx
+++ app/[lng]/(router)/(auth)/login/page.tsx
@@ -1,78 +1,77 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
+import { Button, Input } from "@nextui-org/react";
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { LoginSchema } from '@/validation/login';
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type LoginForm = {
+  email: string
+  password: string
+}
 
 export default function Login() {
+  const { register, handleSubmit, formState: { errors, isSubmitting }, watch } = useForm<LoginForm>({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    resolver: zodResolver(LoginSchema)
+  })
+  const onSubmit: SubmitHandler<LoginForm> = (data) => {
+    console.log({ data });
+
+  }
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
-            </a>
-          </p>
-        </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
       </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            label="Email address"
+            type="email"
+            autoComplete='off'
+            {...register('email')}
+            placeholder="Enter your email"
+          />
+          {errors.email?.message && <span>{errors.email?.message}</span>}
+          <Input
+            label="Password"
+            type="password"
+            {...register('password')}
+            placeholder="Enter your password"
+            autoComplete='off'
+          />
+          {errors.password?.message && <span>{errors.password?.message}</span>}
+          <div>
+            <Button
+              color='primary'
+              type="submit" className="w-full"
+            >
+              Sign in
+            </Button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{' '}
+          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+      <pre>{JSON.stringify(watch(), null, 2)}</pre>
+    </div>
   );
 }
